87ae843d70bab0b1ed5ca703aca44dcc83f6b64e
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Air-2.local>
2025-05-22 19:00:03 +0530

    added health check

--- src/main/java/com/project/uber/uberApp/configs/WebSecurityConfig.java
+++ src/main/java/com/project/uber/uberApp/configs/WebSecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig  {
     private final JWTAuthFilter jwtAuthFilter;
 
-    private static final String[] PUBLIC_ROUTES = {"/auth/**"};
+    private static final String[] PUBLIC_ROUTES = {"/auth/**", "/"};
 
     @Bean
     SecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity) throws Exception {
